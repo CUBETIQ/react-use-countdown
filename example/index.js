d7759ae004e88e9a67f6cee806b587315384f404
S.long <s.long@cubetiqs.com>
2020-08-28 15:40:28 +0700

    update example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,12 @@
-const { sample } = require("lib-name")
+const { sqrt, power } = require("lib-name")
 
-console.log("Example : ")
+console.log("Example Power : ")
 console.log("==========================")
-console.log("Result of 2 = ", sample(2))
-console.log("Result of 4 = ", sample(4))
+console.log("Result of 2 = ", power(2))
+console.log("Result of 4 = ", power(4))
+
+
+console.log("Example Sqrt : ")
+console.log("==========================")
+console.log("Result of 4 = ", sqrt(4))
+console.log("Result of 100 = ", sqrt(100))
