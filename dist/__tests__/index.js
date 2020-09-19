0eb39ff548332aa4aefb33b19f7cd69e9462564f
Sambo Chea <sombochea100@gmail.com>
2020-09-19 16:06:18 +0700

    fixed the publish and remove publish command

--- dist/__tests__/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function sumTest() {
-    return 1 + 4;
-}
-test("test sum with 1 + 4 -> 5", function () {
-    expect(1 + 4).toBe(5);
-});
--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,7 +1,3 @@
-function sumTest() {
-    return 1 + 4;
-}
-
 test(`test sum with 1 + 4 -> 5`, () => {
     expect(1 + 4).toBe(5);
 });
