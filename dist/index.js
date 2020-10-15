3260d16932d027dd4649ac3bb2bd07bed046ef26
Sambo Chea <sombochea@cubetiqs.com>
2020-10-15 10:23:22 +0700

    updated gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useCountdown = void 0;
-var useCountdown_1 = require("./useCountdown");
-Object.defineProperty(exports, "useCountdown", { enumerable: true, get: function () { return __importDefault(useCountdown_1).default; } });
